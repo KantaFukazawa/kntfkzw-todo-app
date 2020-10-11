8ed5f93b67365b4e224ff73ecd10a8f23dcdf872
深澤　寛太 <kantafkzw@gmail.com>
2020-10-11 20:53:00 +0900

    newページ作成、create機能

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,7 +3,22 @@ class BoardsController < ApplicationController
     @boards = Board.all
   end
 
-  def show
-    @board = Board.find(params[:id])
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to boards_path(@board), notice: '保存に成功しました。'
+    else
+      flash.now[:alert] = '保存に失敗しました。'
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
   end
 end
