b928f5c168b7772d40c25deabc7b2b01a414c7b5
深澤　寛太 <kantafkzw@gmail.com>
2020-10-25 14:28:38 +0900

    userとtaskの紐付け。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :show, :create, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -8,10 +8,14 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      redirect_to tasks_path(@task), notice:'保存に成功しました。'
+      redirect_to tasks_path(@task), notice: '保存に成功しました。'
     else
       flash.now[:alert] = '保存に失敗しました。'
       render :new
@@ -19,16 +23,31 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
+    @task = current_user.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
   end
 
   def destroy
+    task = current_user.tasks.find(params[:id])
+    task.destroy!
+    redirect_to tasks_path(@task), notice: '削除しました'
   end
 
   private
     def task_params
-      params.require(:task).permit(:title, :description, :user_id)
+      params.require(:task).permit(:title, :description, :eyecatch, :user_id)
+    end
+
+    def set_task
+      @task = task.find(params[:id])
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
-  belongs_to :user, optional: true
+  has_one_attached :eyecatch
+  belongs_to :user
 end
