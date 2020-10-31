63aed2626cb0c40ab311e044b1677d09c44b8460
深澤　寛太 <kantafkzw@gmail.com>
2020-10-31 22:26:03 +0900

    コメントを投稿、表示、css変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :task
+  belongs_to :user
+  belongs_to :article
+  has_many :comment_images
+  accepts_nested_attributes_for :comment_images
+
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :content, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   root to: "boards#index"
   resources :boards, only: [:index, :new, :create, :edit, :update, :destroy] 
   resources :tasks, only: [:index, :show, :new, :create, :edit, :update, :destroy] 
+
+  resources :tasks do
+    resources :comments, only:[:new, :create]
+  end
 end
