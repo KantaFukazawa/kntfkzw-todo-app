ffc8ad36b48ee794c5584c9fa97178119744ccce
深澤　寛太 <kantafkzw@gmail.com>
2020-11-02 21:42:59 +0900

    プロフィールの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_one :profile, dependent: :destroy
 
+  delegate :birthday, :gender, to: :profile, allow_nil: true
+
   def has_written?(board)
     boards.exists?(id: board.id)
   end
