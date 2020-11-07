cf38c59490db7b6972d3d7ab4ac7d03932268c28
深澤　寛太 <kantafkzw@gmail.com>
2020-11-07 10:04:47 +0900

    カラム名変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,8 @@ class CommentsController < ApplicationController
   end
 
   def create
-    binding.pry
-    task = task.find(params[:id])
+    task = Task.find(params[:task_id])
     @comment = task.comments.build(comment_params)
-
     if @comment.save
       redirect_to task_path(task), notice: 'コメントを追加'
     else
@@ -18,8 +16,11 @@ class CommentsController < ApplicationController
     end
   end
 
+  
+
   private
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_060421) do
+ActiveRecord::Schema.define(version: 2020_11_06_051419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2020_11_01_060421) do
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "User_id"
+    t.integer "user_id"
     t.index ["task_id"], name: "index_comments_on_task_id"
   end
 
