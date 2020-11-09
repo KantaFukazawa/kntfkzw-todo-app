01b24ffa2c749429b60fd05216335dfec89af000
深澤　寛太 <kantafkzw@gmail.com>
2020-11-09 20:10:53 +0900

    修正② - アイキャッチ、link

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,10 @@ class CommentsController < ApplicationController
   end
 
   def create
-    task = Task.find(params[:task_id])
-    @comment = task.comments.build(comment_params)
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.build(comment_params)
     if @comment.save
-      redirect_to task_path(task), notice: 'コメントを追加'
+      redirect_to task_path(@task), notice: 'コメントを追加'
     else
       flash.now[:error] = '更新できませんでした'
       render :new
