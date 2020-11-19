927fa9005dac007370b861a57b2778529bc8b6e8
深澤　寛太 <kantafkzw@gmail.com>
2020-11-19 23:40:03 +0900

    修正１５

--- db/migrate/20201119143852_create_comments.rb
+++ db/migrate/20201119143852_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.references :task, foreign_key: true
-      t.references :user, foreign_key: true
+      t.references :task
+      t.references :user
       t.text :content, null: false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_141037) do
+ActiveRecord::Schema.define(version: 2020_11_19_143852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,5 @@ ActiveRecord::Schema.define(version: 2020_11_19_141037) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "tasks"
-  add_foreign_key "comments", "users"
   add_foreign_key "tasks", "users"
 end
